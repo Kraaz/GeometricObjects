9889380f52f78b5f3aaf2d706f45f026506b250b
Kraaz <kristian.mdal@gmail.com>
2016-10-11 14:32:51 +0200

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,41 +1,59 @@
 public class Circle extends GeometricObject
 {
 	private double radius;
+
+	// Default constructor
 	public Circle ()
 	{
-		super(0.01);
+		
+		super();
 		radius = 1;
 
 	}
+
+	// Constructor med parametre
 	public Circle (double r, double t)
 	{
 		super(t);
 		radius = r;
 
 	}
+	// Gir radius
 	public double getRadius()
 	{
 		return radius;
 	}
+	// Setter radius
 	public void setRadius(double r)
 	{
 		radius = r;
 	}
+
+	//Regner ut areal
 	public double findArea()
 	{
 		return radius * radius * Math.PI;
 	}
+
+	//Regner ut omkrets
 	public double findCircumference()
 	{
 		return 2 * radius * Math.PI;
 	}
-	public String toString()
-	{
-		return "Circle radius= " + radius + super.toString();
-	}
+
+	//Regner ut vekten
 	public double findWeight()
 	{
 		return super.getWeight() * Math.PI * radius * radius * getThickness();
+	}
+	//Inneholder data for sirkelen
+	public String toString()
+	{
+		return "Circle radius: " + radius + "m" +
+		"\nThickness: " + getThickness() + "m" +
+		"\nArea: " + findArea() + "m^3" +
+		"\nCircumference: " + findCircumference() + "m" +
+		"\nWeight: " + findWeight() + "kg"; 
 	}
 
 }
--- TestObject.java
+++ TestObject.java
@@ -7,8 +7,37 @@ public class TestObject
 	{
 		Scanner input = new Scanner(System.in);
 		double x,y;
-		System.out.println("Tast 1 for aa lage en sirkel");
-		if (input.nextInt() == 1)
+		System.out.println("Select an object" + 
+			"\n Type 1 for circle" +  
+			"\n Type 2 for rectangle" + 
+			"\n Type 3 for cylinder" + 
+			"\n Type 4 for cone" + 
+			"\n Type 5 for sphere" + 
+			"\n Type 6 for cuboid" );
+
+		switch (input.nextInt())
+		{
+			case 1: circle(); //objectType = "Circle";
+					
+					break;
+			case 2: //objectType = "Rectangle";
+					break;
+			case 3: //objectType = "Cylinder";
+					break;
+			case 4: //objectType = "Cone";
+					break;
+			case 5: //objectType = "Sphere";
+					break;
+			case 6: //objectType = "Cuboid";
+					break;
+		}
+
+
+		
+
+
+
+		/* (input.nextInt() == 1)
 		{
 			System.out.println("Velg radius i meter: ");
 			x = input.nextDouble();
@@ -16,12 +45,16 @@ public class TestObject
 			y = input.nextDouble();
 
 			Circle circle1 = new Circle(x,y);
-			System.out.println("Sirkelen er ferdig!");
-			System.out.println("Arealet er: " + circle1.findArea() + " m^3");
-			System.out.println("Vekten er: " + circle1.findWeight() + " kg");
+			System.out.println(circle1.toString());
 
-		}
+		}*/
 
 
+	}
+
+	private static void circle()
+	{
+		Circle object = new Circle();
+		System.out.println(object.toString());
 	}
 }
